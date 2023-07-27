9b4460f31e375e24b1d45decd6de67fdcf1b8a1a
Abundance Esim <abundanceesim@gmail.com>
2023-07-26 23:45:31 -0400

    Modify app logo and add partial border

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,36 +1,12 @@
-import { BrowserRouter as Router, Route, Routes, NavLink,Link } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Projects from "../pages/Projects";
-import Artworks from "../pages/Artworks";
-import Contact from "../pages/Contact";
-import NotFound from "../pages/NotFound";
-
-
-// export default function Header() {
-//   return (
-//     <>
-//       <Router>
-//         <div>
-//           <Routes>
-//             <Route path="/" element={<Home />}>home</Route>
-//             <Route path="/projects" element={<Projects />}>projects</Route>
-//             <Route path="/artworks" element={<Artworks />}>artworks</Route>
-//             <Route path="/about" element={<About />}>about</Route>
-//             <Route path="/contact" element={<Contact />}>contact</Route>
-//             <Route path="*" element={<NotFound />} />
-//           </Routes>
-//         </div>
-//       </Router>
-//     </>
-//   );
-// }
+import { BrowserRouter as Router, Route, Routes, NavLink, Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <div className="nav-logo">Logo</div>
+        <div className="nav-logo">
+          <NavLink exact to="/"> AE </NavLink>
+        </div>
         <ul className="nav-menu">
           <li className="nav-item">
             <div className="nav-div-item">
@@ -40,7 +16,7 @@ export default function Header() {
             </div>
           </li>
           <li className="nav-item">
-            <NavLink to="/about" activeClassName="active"> 
+            <NavLink to="/about" activeClassName="active">
               About
             </NavLink>
           </li>
@@ -51,7 +27,7 @@ export default function Header() {
           </li>
           <li className="nav-item">
             <NavLink to="/artworks" activeClassName="active">
-              Artworks
+              Art
             </NavLink>
           </li>
           <li className="nav-item">
