c2bd1af475b2b02a14cabe906eaef54ef8213f6f
Abundance Esim <abundanceesim@gmail.com>
2023-07-31 12:27:13 -0400

    Add artwork structure

---
+++ client/src/components/Artwork.jsx
@@ -0,0 +1,9 @@
+
+export default function Artwork({artwork}) {
+  return (
+    <div>
+      <strong>Description: </strong>
+      {artwork.description}
+    </div>
+  );
+}
--- client/src/pages/Artworks.jsx
+++ client/src/pages/Artworks.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import Artwork from '../components/Artwork'
 
 export default function Artworks() {
+
+  const artworks = [
+    {
+      id: 1,
+      src: "",
+      description: "FIrst artwork",
+    },
+    {
+      id: 2,
+      src: "sdjskdj",
+      description: "Secondddd",
+    },
+    {
+      id: 3,
+      src: "lol",
+      description: "third lol",
+    },
+    {
+      id: 4,
+      src: "sduissd",
+      description: "4th",
+    },
+  ];
+
+  console.log(artworks);
   return (
-    <div>Artworks</div>
-  )
+    <>
+      <div className="art-container">
+        {artworks.map((art) => (
+          <div className="row mt-4">
+            <Artwork artwork={art} key={art.id} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
