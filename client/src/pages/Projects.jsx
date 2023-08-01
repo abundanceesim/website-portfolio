baa877c4e161c9ea0ac0d055b7c0d5cee882eaa6
Abundance Esim <abundanceesim@gmail.com>
2023-08-01 15:21:49 -0400

    Add projects to Projects page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem';
+import goalTracker from '../assets/project-imgs/goal-tracker.png'
+import projManagement from '../assets/project-imgs/project-management.png'
 
 export default function Projects() {
+
+  const projects = [
+    {
+      id: 1,
+      title: "Goal Tracker",
+      description: "MERN stack application to keep track of goals",
+      link: "https://github.com/abundanceesim/goal-tracker",
+      screenshot: goalTracker,
+      technologies: [],
+    },
+    {
+      id: 2,
+      title: "Project Management App",
+      description: "Project management app made with React, GraphQL and Apollo.",
+      link: "https://github.com/abundanceesim/project-management-app",
+      screenshot: projManagement,
+      technologies: [],
+    },
+    {
+      id: 3,
+      title: "Potato Inventory App",
+      description: "",
+      link: "https://github.com/abundanceesim/potato-inventory-app",
+      screenshot: "",
+      technologies: [],
+    },
+    {
+      id: 4,
+      title: "Products Web App",
+      description: "",
+      link: "https://github.com/abundanceesim/products-web-app",
+      screenshot: "",
+      technologies: [],
+    },
+  ];
+
   return (
-    <div>Projects</div>
+    <>
+      {projects.length > 0 ? (
+        <div className='container'>
+          <div className="row">
+            {projects.map((project) => (
+              <div className="col-6 mb-4">
+                <ProjectItem project={project}/>
+              </div>
+            ))}
+          </div>
+        </div>
+      ): (
+        <h3>No projects found.</h3>
+      )}
+    </>
+    
   )
 }
