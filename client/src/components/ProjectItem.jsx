9ff8b4dbb58de50cce8d640a9ae9864beb3695c7
Abundance Esim <abundanceesim@gmail.com>
2023-08-03 18:55:25 -0400

    Modify About page

--- client/src/components/ProjectItem.jsx
+++ client/src/components/ProjectItem.jsx
@@ -4,15 +4,19 @@ function ProjectItem({project}) {
   return (
     <>
       <div className="project-container">
-        <div className="project-img" >
-          <img src={project.screenshot} alt={project.shortTitle + "-img"}  />
+        <div className="project-img">
+          <img src={project.screenshot} alt={project.shortTitle + "-img"} />
         </div>
       </div>
-      <div className='project-description'>
+      <div className="project-description">
         <h5>{project.title}</h5>
         {project.description}
       </div>
-      <div><a className='btn btn-primary' href={project.link}>GITHUB</a></div>
+      <div className='github-btn'>
+        <a className="btn btn-outline-primary github-source-btn" href={project.link}>
+          GITHUB
+        </a>
+      </div>
     </>
   );
 }
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='container'>Hi, I'm Abundance, a software developer and recent Computer Programming 
+      graduate.
+    </div>
   )
 }
