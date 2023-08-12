5a9c7efd46e83d9127857c682dd5842e5a1f69d0
Abundance Esim <abundanceesim@gmail.com>
2023-08-11 21:41:20 -0400

    Limit number of projects and artworks on Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,24 +8,39 @@ import Artworks from "./Artworks";
 export default function Home() {
   return (
     <>
-      <div className="intro">
+      <div className="container">
+        <div className="intro">
         <div>Hi there, I'm Abundance</div>
         <div>Software developer by day, artist by night</div>
         <div className="github-btn">
-          <a
-            className="btn btn-outline-primary github-source-btn"
-            href="/contact"
-          >
-            SAY HI
-          </a>
+          <a className="btn btn-outline-primary github-source-btn"
+            href="/contact" >CONNECT</a>
         </div>
       </div>
       <div className="recent-projects">
-        <Projects />
+        {/* <div className="container projects"> */}
+          <div className="row">
+            {projects.filter((item, idx) => idx < 2).map((project) => (
+              <div className="col mb-5" style={{ maxWidth: "min-content" }}>
+                <ProjectItem project={project} key={project} />
+              </div>
+            ))}
+          </div>
+        {/* </div> */}
       </div>
       <div className="recent-artworks">
-        <Artworks/>
+        {/* <div className="art-page"> */}
+        <div className="row mt-4">
+          {artworks.filter((item, idx) => idx < 3).map((art) => (
+            <div className="col mb-3">
+              <Artwork artwork={art} key={art.id} />
+            </div>
+          ))}
+        </div>
+      {/* </div> */}
       </div>
+      </div>
+      
     </>
   );
 }
