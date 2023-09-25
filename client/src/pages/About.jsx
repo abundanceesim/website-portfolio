93437eb8239210d6e6429a7ab15806504b1092a2
Abundance Esim <abundanceesim@gmail.com>
2023-09-25 14:57:24 -0400

    Modify about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,32 +10,36 @@ export default function About() {
             <img className="profile-picture" src={me} alt="abundance-esim" />
           </div>
           <div className="col">
-            Hi, I'm Abundance Esim, a software developer and recent Computer
-            Programming graduate. I'm Nigerian but reside in Canada. I
-            have experience building web applications, particularly using
-            JavaScript, Angular and React. Asides from these, I'm proficient in
-            Java.
+            <div className="about-txt about-txt-primary">
+              Hi, I'm Abundance Esim, a software developer and recent Computer
+              Programming graduate. I'm Nigerian but reside in Canada. I have
+              experience building web applications, particularly using
+              JavaScript, Angular and React. Asides from these, I'm proficient
+              in Java.
+            </div>
           </div>
         </div>
-        When I'm not immersed in lines of code, you'll likely find me exploring
-        new art mediums, either digitally or traditionally. I also really enjoy
-        playing online multiplayer games like Apex Legends. I firmly believe in
-        the power of work-life balance, and these pursuits keep me energized and
-        inspired.
-        <div>
-          If my profile matches the description of someone you'd like to join
-          your team, feel free to{" "}
-          <span>
-            <a href="/contact">connect</a>
-          </span>
-          , otherwise, you can check out my{" "}
-          <span>
-            <a href="/projects">projects</a>
-          </span>{" "}
-          or{" "}
-          <span>
-            <a href="/artworks">art.</a>
-          </span>
+        <div className="about-txt about-txt-secondary">
+          When I'm not immersed in lines of code, you'll likely find me
+          exploring new art mediums, either digitally or traditionally. I also
+          really enjoy playing online multiplayer games like Apex Legends. I
+          firmly believe in the power of work-life balance, and these pursuits
+          keep me energized and inspired.
+          <div>
+            If my profile matches the description of someone you'd like to join
+            your team, feel free to{" "}
+            <span>
+              <a href="/contact">connect</a>
+            </span>
+            , otherwise, you can check out my{" "}
+            <span>
+              <a href="/projects">projects</a>
+            </span>{" "}
+            or{" "}
+            <span>
+              <a href="/artworks">art.</a>
+            </span>
+          </div>
         </div>
       </div>
     </>
