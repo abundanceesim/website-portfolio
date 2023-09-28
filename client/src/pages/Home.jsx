15e1b284e8f5f6c834722b422c955b0f3aa0888c
Abundance Esim <abundanceesim@gmail.com>
2023-09-28 02:23:12 -0400

    Add github and linkedin links to Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Artworks from "./Artworks";
 // import me from '../assets/personal/abundance_transparent_bg.png'
 import me from "../assets/personal/abundance-snow.png";
 // import me from "../assets/personal/abundance-boba.png";
+import {FaLinkedin, FaGithub} from 'react-icons/fa';
 
 
 export default function Home() {
@@ -15,17 +16,29 @@ export default function Home() {
       <div className="introContent">
         <span className="hello">Hello, </span>
         <span className="introText">
-          I'm <span className="introName">Abundance</span>. <br /> Software Developer
+          I'm <span className="introName">Abundance</span>. <br /> Software
+          Developer
         </span>
         <p className="introPara">
           I am a skilled full-stack developer with experience creating visually
           appealing and user friendly web applications
         </p>
-          <a
-            className="mt-3 btn btn-outline-primary github-source-btn connect-btn"
-            style={{ width: "fit-content" }}
-            href="/contact"
-          >SAY HI</a>
+        <a
+          className="mt-3 btn btn-outline-primary github-source-btn connect-btn"
+          style={{ width: "fit-content" }}
+          href="/contact"
+        >
+          SAY HI
+        </a>
+        <div className="d-flex gap-2 align-items-center">
+          <a href="http://www.linkedin.com/in/abundance-esim">
+            <FaLinkedin className="mt-3" style={{ color: "white" }} />
+          </a>
+          <a href="https://github.com/abundanceesim">
+            <FaGithub className="mt-3" style={{ color: "white" }} />
+          </a>
+        </div>
+
         <img className="bg" src={me} alt="abundance-esim" />
       </div>
       {/* <div className="container">
