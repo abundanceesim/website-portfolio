4d5fb7fd20c944e5065a4ec0c9c1796c942756a0
Abundance Esim <abundanceesim@gmail.com>
2023-09-30 20:50:34 -0400

    Add hamburger menu to navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, NavLink, Link } from "react-router-dom";
+import menu from '../assets/misc/menu.png'
 
 export default function Header() {
+
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
-    <nav className="navbar">
+    <nav className="navbar" style={{overflow:'none !important'}}>
       <div className="nav-container">
         <div className="nav-logo">
           <NavLink exact to="/">
@@ -10,23 +15,45 @@ export default function Header() {
             AE.{" "}
           </NavLink>
         </div>
-        <ul className="nav-menu">
-          <NavLink exact to="/" activeClassName="active">
-            <li className="nav-item">Home</li>
-          </NavLink>
-          <NavLink exact to="/about" activeClassName="active">
-            <li className="nav-item">About</li>
-          </NavLink>
-          <NavLink exact to="/projects" activeClassName="active">
-            <li className="nav-item">Projects</li>
-          </NavLink>
-          <NavLink exact to="/artworks" activeClassName="active">
-            <li className="nav-item">Art</li>
-          </NavLink>
-          <NavLink exact to="/contact" activeClassName="active">
-            <li className="nav-item">Contact</li>
-          </NavLink>
-        </ul>
+        
+          <ul className="nav-menu">
+            <NavLink exact to="/" activeClassName="active">
+              <li className="nav-item">Home</li>
+            </NavLink>
+            <NavLink exact to="/about" activeClassName="active">
+              <li className="nav-item">About</li>
+            </NavLink>
+            <NavLink exact to="/projects" activeClassName="active">
+              <li className="nav-item">Projects</li>
+            </NavLink>
+            <NavLink exact to="/artworks" activeClassName="active">
+              <li className="nav-item">Art</li>
+            </NavLink>
+            <NavLink exact to="/contact" activeClassName="active">
+              <li className="nav-item">Contact</li>
+            </NavLink>
+          </ul>
+          <div className="mobile-menu">
+            <img src={menu} alt="" className='mobMenu' onClick={()=>setShowMenu(!showMenu)} style={{display: 'none'}}/>
+            <ul className="nav-menu" style={{display:showMenu ? 'flex': 'none'}}>
+              <NavLink exact to="/" activeClassName="active"  onClick={()=>setShowMenu(false)}>
+                <li className="mob-nav-item">Home</li>
+              </NavLink>
+              <NavLink exact to="/about" activeClassName="active" onClick={()=>setShowMenu(false)}>
+                <li className="mob-nav-item">About</li>
+              </NavLink>
+              <NavLink exact to="/projects" activeClassName="active" onClick={()=>setShowMenu(false)}>
+                <li className="mob-nav-item">Projects</li>
+              </NavLink>
+              <NavLink exact to="/artworks" activeClassName="active" onClick={()=>setShowMenu(false)}>
+                <li className="mob-nav-item">Art</li>
+              </NavLink>
+              <NavLink exact to="/contact" activeClassName="active" onClick={()=>setShowMenu(false)}>
+                <li className="mob-nav-item">Contact</li>
+              </NavLink>
+            </ul>
+          </div>
+        
       </div>
     </nav>
   );
