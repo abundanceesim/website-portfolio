b455051cc05583e7ef2d93b8f305c20a3716eec4
Abundance Esim <abundanceesim@gmail.com>
2023-10-02 13:56:59 -0400

    Modify mobile nav-menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, NavLink, Link } from "react-router-dom";
-import menu from '../assets/misc/menu.png'
 import { Squash as Hamburger } from 'hamburger-react'
 
 export default function Header() {
@@ -8,54 +7,85 @@ export default function Header() {
   const [showMenu, setShowMenu] = useState(false)
 
   return (
-    <nav className="navbar" style={{overflow:'none !important'}}>
+    <nav className="navbar" style={{ overflow: "none !important" }}>
       <div className="nav-container">
         <div className="nav-logo">
-          <NavLink exact to="/">
+          <NavLink exact to="/" onClick={() => setShowMenu(false)}>
             {" "}
             AE.{" "}
           </NavLink>
         </div>
-        
-          <ul className="nav-menu">
-            <NavLink exact to="/" activeClassName="active">
-              <li className="nav-item">Home</li>
+
+        <ul className="nav-menu">
+          <NavLink exact to="/" activeClassName="active">
+            <li className="nav-item">Home</li>
+          </NavLink>
+          <NavLink exact to="/about" activeClassName="active">
+            <li className="nav-item">About</li>
+          </NavLink>
+          <NavLink exact to="/projects" activeClassName="active">
+            <li className="nav-item">Projects</li>
+          </NavLink>
+          <NavLink exact to="/artworks" activeClassName="active">
+            <li className="nav-item">Art</li>
+          </NavLink>
+          <NavLink exact to="/contact" activeClassName="active">
+            <li className="nav-item">Contact</li>
+          </NavLink>
+        </ul>
+        <div className="mobile-menu">
+          <Hamburger
+            className="hamburger-react mobMenu"
+            toggled={showMenu}
+            toggle={setShowMenu}
+          />
+          {/* <img src={menu} alt="" className='mobMenu' onClick={()=>setShowMenu(!showMenu)} style={{display: 'none'}}/> */}
+          <ul
+            className="nav-menu"
+            style={{ display: showMenu ? "flex" : "none" }}
+          >
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              <li className="mob-nav-item">Home</li>
             </NavLink>
-            <NavLink exact to="/about" activeClassName="active">
-              <li className="nav-item">About</li>
+            <NavLink
+              exact
+              to="/about"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              <li className="mob-nav-item">About</li>
             </NavLink>
-            <NavLink exact to="/projects" activeClassName="active">
-              <li className="nav-item">Projects</li>
+            <NavLink
+              exact
+              to="/projects"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              <li className="mob-nav-item">Projects</li>
             </NavLink>
-            <NavLink exact to="/artworks" activeClassName="active">
-              <li className="nav-item">Art</li>
+            <NavLink
+              exact
+              to="/artworks"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              <li className="mob-nav-item">Art</li>
             </NavLink>
-            <NavLink exact to="/contact" activeClassName="active">
-              <li className="nav-item">Contact</li>
+            <NavLink
+              exact
+              to="/contact"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              <li className="mob-nav-item">Contact</li>
             </NavLink>
           </ul>
-          <div className="mobile-menu">
-            <Hamburger className='hamburger-react mobMenu' toggled={showMenu} toggle={setShowMenu}/>
-            {/* <img src={menu} alt="" className='mobMenu' onClick={()=>setShowMenu(!showMenu)} style={{display: 'none'}}/> */}
-            <ul className="nav-menu" style={{display:showMenu ? 'flex': 'none'}}>
-              <NavLink exact to="/" activeClassName="active"  onClick={()=>setShowMenu(false)}>
-                <li className="mob-nav-item">Home</li>
-              </NavLink>
-              <NavLink exact to="/about" activeClassName="active" onClick={()=>setShowMenu(false)}>
-                <li className="mob-nav-item">About</li>
-              </NavLink>
-              <NavLink exact to="/projects" activeClassName="active" onClick={()=>setShowMenu(false)}>
-                <li className="mob-nav-item">Projects</li>
-              </NavLink>
-              <NavLink exact to="/artworks" activeClassName="active" onClick={()=>setShowMenu(false)}>
-                <li className="mob-nav-item">Art</li>
-              </NavLink>
-              <NavLink exact to="/contact" activeClassName="active" onClick={()=>setShowMenu(false)}>
-                <li className="mob-nav-item">Contact</li>
-              </NavLink>
-            </ul>
-          </div>
-        
+        </div>
       </div>
     </nav>
   );
