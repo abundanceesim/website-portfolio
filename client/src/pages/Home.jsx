5148f2561628decb3de6c3d03d894b8dc914afc0
Abundance Esim <abundanceesim@gmail.com>
2023-10-01 20:13:33 -0400

    Enable home buttons for mobile view

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,20 +23,25 @@ export default function Home() {
           I am a skilled full-stack developer with experience creating visually
           appealing and user friendly web applications.
         </p>
-        <a
-          className="mt-2 btn btn-outline-primary github-source-btn connect-btn"
-          style={{ width: "fit-content" }}
-          href="/contact"
-        >
-          SAY HI
-        </a>
-        <div className="d-flex gap-2 align-items-center">
-          <a href="http://www.linkedin.com/in/abundance-esim">
-            <FaLinkedin className="mt-3 home-icon" style={{ color: "white" }} />
+        <div className="home-btns">
+          <a
+            className="mt-2 btn btn-outline-primary github-source-btn connect-btn"
+            style={{ width: "fit-content" }}
+            href="/contact"
+          >
+            SAY HI
           </a>
-          <a href="https://github.com/abundanceesim">
-            <FaGithub className="mt-3 home-icon" style={{ color: "white" }} />
-          </a>
+          <div className="d-flex gap-2 align-items-center">
+            <a href="http://www.linkedin.com/in/abundance-esim">
+              <FaLinkedin
+                className="mt-3 home-icon"
+                style={{ color: "white" }}
+              />
+            </a>
+            <a href="https://github.com/abundanceesim">
+              <FaGithub className="mt-3 home-icon" style={{ color: "white" }} />
+            </a>
+          </div>
         </div>
 
         <img className="bg" src={me} alt="abundance-esim" />
